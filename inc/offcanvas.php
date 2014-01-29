093ba1531bef704c44132e55b02600c3c18438e0
rediris <romanedirisinghe@gmail.com>
2014-01-29 15:16:22 -0500

    added new offcanvas include

---
+++ inc/offcanvas.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Off-Canvas Setup
+ *
+ * Setup theme customization options for Foundation's Off-Canvas Navigation
+ *
+ * @package WordPress
+ * @subpackage Infrastrukt for WordPress
+ * @since Infrastrukt for WordPress 1.0
+ */
+
+
+?>
